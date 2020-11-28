49969647ecd1eb9238b3f262040d70759798a4bb
Purusottam <purusottamadhikari234@gmail.com>
2020-11-28 21:01:56 +0545

    first commit

---
+++ orders/urls.py
@@ -0,0 +1,8 @@
+from orders.views import order_create
+from django.urls import path
+from .import views
+app_name = 'orders'
+
+urlpatterns = [
+    path('create/', views.order_create, name='order_create'),
+]
